b59cae684ca10d85e29d5bbe9cc48d0b6fb0703c
Sam <semiddleton1@live.com>
2023-04-20 15:14:30 -0500

    Made the webgl

---
+++ Assets/Scripts/QuitBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitBehaviour : MonoBehaviour   
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
